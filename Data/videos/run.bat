6a0cabf9b22ca00dde72aa60f549f7e23fd5f1d4
Tiago Nobrega <tiagonobrega80@gmail.com>
2025-05-07 15:00:26 +0100

    alguns videos corrigidos

--- Data/videos/run.bat
+++ Data/videos/run.bat
@@ -1,18 +1,14 @@
 @echo off
-echo == Penalty Cutter - CRO vs DIN - WC2018 (vídeo apenas) ==
+echo == Penalty Cutter - BRA vs CRO - WC2022 (vídeo apenas) ==
 echo.
 
-yt-dlp "https://www.youtube.com/watch?v=lO36Q8Uj2bE" --cookies cookies.txt --download-sections "*00:00:52-00:00:58" -o "cro-din-wc2018-1.mp4" -f "bv*" --recode-video mp4
-yt-dlp "https://www.youtube.com/watch?v=lO36Q8Uj2bE" --cookies cookies.txt --download-sections "*00:01:44-00:01:47" -o "cro-din-wc2018-2.mp4" -f "bv*" --recode-video mp4
-yt-dlp "https://www.youtube.com/watch?v=lO36Q8Uj2bE" --cookies cookies.txt --download-sections "*00:02:39-00:02:42" -o "cro-din-wc2018-3.mp4" -f "bv*" --recode-video mp4
-yt-dlp "https://www.youtube.com/watch?v=lO36Q8Uj2bE" --cookies cookies.txt --download-sections "*00:03:22-00:03:26" -o "cro-din-wc2018-4.mp4" -f "bv*" --recode-video mp4
-yt-dlp "https://www.youtube.com/watch?v=lO36Q8Uj2bE" --cookies cookies.txt --download-sections "*00:04:17-00:04:20" -o "cro-din-wc2018-5.mp4" -f "bv*" --recode-video mp4
-yt-dlp "https://www.youtube.com/watch?v=lO36Q8Uj2bE" --cookies cookies.txt --download-sections "*00:05:03-00:05:06" -o "cro-din-wc2018-6.mp4" -f "bv*" --recode-video mp4
-yt-dlp "https://www.youtube.com/watch?v=lO36Q8Uj2bE" --cookies cookies.txt --download-sections "*00:05:53-00:05:57" -o "cro-din-wc2018-7.mp4" -f "bv*" --recode-video mp4
-yt-dlp "https://www.youtube.com/watch?v=lO36Q8Uj2bE" --cookies cookies.txt --download-sections "*00:07:00-00:07:06" -o "cro-din-wc2018-8.mp4" -f "bv*" --recode-video mp4
-yt-dlp "https://www.youtube.com/watch?v=lO36Q8Uj2bE" --cookies cookies.txt --download-sections "*00:07:52-00:07:58" -o "cro-din-wc2018-9.mp4" -f "bv*" --recode-video mp4
-yt-dlp "https://www.youtube.com/watch?v=lO36Q8Uj2bE" --cookies cookies.txt --download-sections "*00:08:50-00:08:54" -o "cro-din-wc2018-10.mp4" -f "bv*" --recode-video mp4
+yt-dlp "https://www.youtube.com/watch?v=uR9vgLLDhE0" --cookies cookies.txt --download-sections "*00:00:15-00:00:17" -o "bra-cro-wc2022-1.mp4" -f "bv*" --recode-video mp4
+yt-dlp "https://www.youtube.com/watch?v=uR9vgLLDhE0" --cookies cookies.txt --download-sections "*00:01:51-00:01:52" -o "bra-cro-wc2022-3.mp4" -f "bv*" --recode-video mp4
+yt-dlp "https://www.youtube.com/watch?v=uR9vgLLDhE0" --cookies cookies.txt --download-sections "*00:02:29-00:02:32" -o "bra-cro-wc2022-4.mp4" -f "bv*" --recode-video mp4
+yt-dlp "https://www.youtube.com/watch?v=uR9vgLLDhE0" --cookies cookies.txt --download-sections "*00:03:16-00:03:20" -o "bra-cro-wc2022-5.mp4" -f "bv*" --recode-video mp4
+yt-dlp "https://www.youtube.com/watch?v=uR9vgLLDhE0" --cookies cookies.txt --download-sections "*00:03:58-00:04:03" -o "bra-cro-wc2022-6.mp4" -f "bv*" --recode-video mp4
+yt-dlp "https://www.youtube.com/watch?v=uR9vgLLDhE0" --cookies cookies.txt --download-sections "*00:04:43-00:04:48" -o "bra-cro-wc2022-7.mp4" -f "bv*" --recode-video mp4
 
 echo.
-echo ✅ Penáltis CRO vs DIN extraídos com sucesso (vídeo apenas)!
+echo ✅ Penáltis BRA vs CRO extraídos (vídeo apenas)!
 pause >nul
